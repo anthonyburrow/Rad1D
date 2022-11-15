fd9993a3420c59fdd0b26afc94db31374a2da30d
Sarah Stangl <sarahstangl@pn2036394.lanl.gov>
2022-11-15 12:50:20 -0700

    tried reformatting so ali and lambdaIterate was seperated and could be switched between them

--- src/lambdaIterate.cpp
+++ src/lambdaIterate.cpp
@@ -15,11 +15,41 @@ namespace myLib
     void calcJ(NuModel &nuModel)
     {
         const double &nZones = nuModel.params.nZones;
+        const vector<vector<double>> &lambda = nuModel.lambda;
+        double jTerm;
+
+        for (int i=0; i < nZones; i++)
+        {
+            jTerm = 0.0;
+            for (int j=0; j < nZones; j++)
+            {
+                jTerm += lambda[i][j] * nuModel.S[j];
+            }
+
+            nuModel.J[i] = jTerm;
+        }
+    }
+
+    void calcS(NuModel &nuModel)
+    {
+        const int &nZones = nuModel.params.nZones;
+        const double &eps = nuModel.params.eps;
+
+        for (int i=0; i < nZones; i++)
+        {
+            nuModel.S[i] = eps * nuModel.B[i] + (1.0 - eps) * nuModel.J[i];
+        }
+    }
+
+
+    //SMS moronic attempts at coding
+    void ALIcalcJ(NuModel &nuModel)
+    {
+        const double &nZones = nuModel.params.nZones;
         const double &eps = nuModel.params.eps;
         const vector<vector<double>> &lambda = nuModel.lambda;
         double jTerm;
         
-        //SMS start edits
         std::vector<double> Jold = nuModel.J;
         std::vector<double> a(nZones-1,0.0);
         std::vector<double> b(nZones,0.0);
@@ -58,24 +88,60 @@ namespace myLib
         {
             nuModel.J[i] = y[i] - Jold[i];
         }        
-        //SMS end edits
     }
 
-    void calcS(NuModel &nuModel)
+    //SMS moronic attempts at coding
+    void ALIcalcS(NuModel &nuModel)
     {
         const int &nZones = nuModel.params.nZones;
         const double &eps = nuModel.params.eps;
 
+        const vector<vector<double>> &lambda = nuModel.lambda;
+        std::vector<double> Sold = nuModel.S;
+        std::vector<double> a(nZones-1,0.0);
+        std::vector<double> b(nZones,0.0);
+        std::vector<double> c(nZones-1,0.0);
+        std::vector<double> x(nZones,0.0);
+        std::vector<double> y(nZones,0.0);
+
         for (int i=0; i < nZones; i++)
         {
-            nuModel.S[i] = eps * nuModel.B[i] + (1.0 - eps) * nuModel.J[i];
+            for (int j=0; j < nZones; j++)
+            {
+                if(i==j)
+                {
+                    b[i]=1.0-(1.0-eps)*lambda[i][j];
+                }
+                if(i==j+1)
+                {
+                    a[i]=1.0-(1.0-eps)*lambda[i][j];
+                }
+                if(i==j-1)
+                {
+                    c[i]=1.0-(1.0-eps)*lambda[i][j];
+                }
+            }
+            x[i] = eps * nuModel.B[i];
         }
+
+        TridiagonalSoln(y, a, b, c, x);
+
+        for (int i=0; i < nZones; i++)
+        {
+            nuModel.S[i] = y[i] - Sold[i];
+        } 
     }
 
     void lambdaIteration(NuModel &nuModel)
     {
         calcJ(nuModel);
-
         calcS(nuModel);
+    }
+
+    //SMS moronic attempts at coding
+    void ALI(NuModel &nuModel)
+    {
+        ALIcalcJ(nuModel);
+        ALIcalcS(nuModel);
     }
 }
