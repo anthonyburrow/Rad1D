6576390ee5b0fdd835ddc214195923b239c06edd
Anthony Burrow <anthony.r.burrow@gmail.com>
2022-12-04 12:57:51 -0600

    cleanify tests

--- tests/test_epsilon.py
+++ tests/test_epsilon.py
@@ -5,131 +5,66 @@ from Rad1D import RadModel
 from .conftest import test_plot_dir
 
 
+params = {
+    'data_dir'    : '/home/masamune/.bin/Rad1D/data',
+    'tau_min'     : 1e-8,
+    'tau_max'     : 1e6,
+    'max_iter'    : 1000,
+    'eps_converge': 1e-7,
+    'verbose'     : False,
+}
+
+eps_to_check = (1e-2, 1e-4, 1e-6)
+
+
+def plot_eps(ax, eps):
+    params['eps'] = eps
+
+    model = RadModel(params)
+    results = model.convergence_test()
+
+    J = (results - eps) / (1 - eps)
+    diffs = np.abs(J[1:] - J[:-1]) / J[:-1]
+    max_diffs = diffs.max(axis=1)
+
+    eps_converge = params['eps_converge']
+    max_iter = params['max_iter']
+    for i in range(max_iter):
+        if max_diffs[i] > eps_converge:
+            continue
+        ind_needed = i
+        break
+    else:
+        ind_needed = max_iter
+
+    results = results[:ind_needed]
+
+    ax.plot(model.tau, results[0], c='r', ls='-', lw=1., alpha=0.4)
+    for i in range(1, ind_needed):
+        ax.plot(model.tau, results[i], c='k', ls='-', lw=1., alpha=0.4)
+
+    ax.axhline(np.sqrt(eps), ls='--', color='k')
+
+    ax.text(0.65, 0.1, f'{ind_needed + 1} iterations', transform=ax.transAxes)
+
+
 def test_epsilon():
-    params = {
-        'data_dir'    : '/Users/adammoss/Desktop/Atmospheres/Rad1Dtest/data',
-        'wave_start'  : 4000.,
-        'wave_end'    : 7000.,
-        'cont_res'    : 0.05,
-        'line_res'    : 3.0,
-        'tau_min'     : 1e-8,
-        'tau_max'     : 1e6,
-        'eps'         : 1e-4,
-        'T_eff'       : 6000.,
-        'n_zones'     : 256,
-        'max_iter'    : 1000,
-        'accelerated' : True,
-        'eps_converge': 1e-7,
-        'n_quad'      : 32,
-        'verbose'     : True,
-    }
+    fig, ax = plt.subplots(1, 3, figsize=(12, 4), sharey=True)
 
-    n_iter=params['max_iter']
-    n_zones=params['n_zones']
-    params['eps'] = 1e-2
-    eps=params['eps']
-    eps_converge=params['eps_converge']
-    eps_con_two = np.sqrt(1e-2)
-    model = RadModel(params)
-    two_results = model.convergence_test()
-    J = (two_results - eps) / (1 - eps)
-    diff = np.abs(J[1:] - J[:-1]) / J[:-1]
-    k=0
-    for i in range(n_iter-1):
-        for j in range(n_zones):
-            if diff[i,j] < eps_converge:
-               k+=1
-            if k == n_zones:
-                print(i)
-                two_correct = two_results[0:i]
-                break
-        if k == n_zones:
-            break
-        k=0 
+    plot_eps(ax[0], 1e-2)
+    plot_eps(ax[1], 1e-4)
+    plot_eps(ax[2], 1e-6)
 
-    params['eps'] = 1e-4
-    eps=params['eps']
-    eps_con_four = np.sqrt(1e-4)
-    model = RadModel(params)
-    four_results = model.convergence_test()
-    J = (four_results - eps) / (1 - eps)
-    diff = np.abs(J[1:] - J[:-1]) / J[:-1]
-    k=0
-    for i in range(n_iter-1):
-        for j in range(n_zones):
-            if diff[i,j] < eps_converge:
-               k+=1
-            if k == n_zones:
-                print(i)
-                four_correct = four_results[0:i]
-                break
-        if k == n_zones:
-            break
-        k=0  
+    [_ax.set_xlim(params['tau_min'], params['tau_max']) for _ax in ax]
+    bottom_convergence = np.sqrt(np.array(eps_to_check).min())
+    ax[0].set_ylim(0.8 * bottom_convergence, 1.1)
 
-    params['eps'] = 1e-6
-    eps=params['eps']
-    eps_con_six = np.sqrt(1e-6)
-    model = RadModel(params)
-    six_results = model.convergence_test()
-    J = (six_results - eps) / (1 - eps)
-    diff = np.abs(J[1:] - J[:-1]) / J[:-1]
-    k=0
-    for i in range(n_iter-1):
-        for j in range(n_zones):
-            if diff[i,j] < eps_converge:
-               k+=1
-            if k == n_zones:
-                print(i)
-                six_correct = six_results[0:i]
-                break
-        if k == n_zones:
-            break
-        k=0 
+    [_ax.set_xscale('log') for _ax in ax]
+    ax[0].set_yscale('log')
 
+    [_ax.set_xlabel(r'$\tau$') for _ax in ax]
+    ax[0].set_ylabel('S / B')
 
-    fig, ax = plt.subplots(dpi=125)
-    ax.plot(model.tau[1:], two_correct[0, 1:], c='r', ls='-', lw=1., alpha=0.4,label='1e-2,115 iterations')
-    for i in range(1, len(two_correct)):
-        ax.plot(model.tau[1:], two_correct[i, 1:], c='k', ls='-', lw=1.,
-                     alpha=0.4)
-    ax.set_xscale('log')
-    ax.set_yscale('log')
-    ax.set_xlabel(r'$\tau$')
-    ax.set_ylabel('S/B')
-    ax.legend(loc='best')
-    ax.axhline(eps_con_two, ls='--', color='r')
     plt.tight_layout()
-    fn = f'{test_plot_dir}/eps2.png'
-    fig.savefig(fn, dpi=125)
-  
-    fig, ax = plt.subplots(dpi=125)
-    ax.plot(model.tau[1:], four_correct[0, 1:], c='r', ls='-', lw=1., alpha=0.4, label='1e-4, 372 iterations')
-    for i in range(1, len(four_correct)):
-        ax.plot(model.tau[1:], four_correct[i, 1:], c='k', ls='-', lw=1.,
-                     alpha=0.4)
-    ax.set_xscale('log')
-    ax.set_yscale('log')
-    ax.set_xlabel(r'$\tau$')
-    ax.set_ylabel('S/B')
-    ax.legend(loc='best')
-    ax.axhline(eps_con_four, ls='--', color='r')
-    plt.tight_layout()
-    fn = f'{test_plot_dir}/eps4.png'
-    fig.savefig(fn, dpi=125)
-  
-    fig, ax = plt.subplots(dpi=125)
-    ax.plot(model.tau[1:], six_correct[0, 1:], c='r', ls='-', lw=1., alpha=0.4,label='1e-6, 605 iterations')
-    for i in range(1, len(six_correct)):
-        ax.plot(model.tau[1:], six_correct[i, 1:], c='k', ls='-', lw=1.,
-                     alpha=0.4)
-    ax.set_xscale('log')
-    ax.set_yscale('log')
-    ax.set_xlabel(r'$\tau$')
-    ax.set_ylabel('S/B')
-    ax.legend(loc='best')
-    ax.axhline(eps_con_six, ls='--', color='r')
-    plt.tight_layout()
-    fn = f'{test_plot_dir}/eps6.png'
-    fig.savefig(fn, dpi=125)
-                              	
+    fn = f'{test_plot_dir}/eps_convergence.pdf'
+    fig.savefig(fn)                              	
--- tests/test_quad.py
+++ tests/test_quad.py
@@ -7,97 +7,48 @@ from .conftest import test_plot_dir
 
 def test_quad():
     params = {
-        'data_dir'    : '/Users/adammoss/Desktop/Atmospheres/Rad1Dtest/data',   # Absolute path to line list
-        'wave_start'  : 4000.,       # Starting wavelength
-        'wave_end'    : 7000.,       # Ending wavelength
-        'cont_res'    : 0.05,        # Points per angstrom resolved for continuum
-        'line_res'    : 3.0,         # Points per angstrom resolved for lines in line list
-        'tau_min'     : 1e-8,        # Minimum tau of atmosphere for continuum
-        'tau_max'     : 1e6,         # Maximum tau of atmosphere for continuum
-        'eps'         : 1e-4,        # Thermalization factor
-        'T_eff'       : 6000.,       # Characteristic temperature of atmosphere
-        'n_zones'     : 256,         # Number of tau points
-        'max_iter'    : 200,         # Maximum number of lambda iterations allowed
-        'accelerated' : True,        # Use ALI algorithm for faster convergence
-        'eps_converge': 1e-7,        # Factor to determine J is converged
-        'n_quad'      : 32,          # Order of Gaussian quadrature integration
-        'verbose'     : True,        # Display stdout output
+        'data_dir'    : '/home/masamune/.bin/Rad1D/data',
+        'tau_min'     : 1e-8,
+        'tau_max'     : 1e6,
+        'eps'         : 1e-4,
+        'verbose'     : False,
     }
 
     params['n_quad'] = 2
     model = RadModel(params)
-    two_results = model.convergence_test()
-    two_final = two_results[-1]
+    final_iter2 = model.convergence_test()[-1]
 
     params['n_quad'] = 4
     model = RadModel(params)
-    four_results = model.convergence_test()
-    four_final = four_results[-1]
+    final_iter4 = model.convergence_test()[-1]
 
     params['n_quad'] = 8
     model = RadModel(params)
-    eight_results = model.convergence_test()
-    eight_final = eight_results[-1]
+    final_iter8 = model.convergence_test()[-1]
 
     params['n_quad'] = 32
     model = RadModel(params)
-    thirtytwo_results = model.convergence_test()
-    thirtytwo_final = thirtytwo_results[-1]
+    final_iter32 = model.convergence_test()[-1]
 
-    two_res=[]
-    four_res=[]
-    eight_res=[]
-    two_perc=[]
-    four_perc=[]
-    eight_perc=[]
-    for i in range(len(thirtytwo_final)):
-        two_res.append(two_final[i]-thirtytwo_final[i])
-        four_res.append(four_final[i]-thirtytwo_final[i])
-        eight_res.append(eight_final[i]-thirtytwo_final[i])
-        two_perc.append((two_res[i]/thirtytwo_final[i])*100)
-        four_perc.append((four_res[i]/thirtytwo_final[i])*100)
-        eight_perc.append((eight_res[i]/thirtytwo_final[i])*100)
+    pct_diff2 = (final_iter2 - final_iter32) * 100. / final_iter32
+    pct_diff4 = (final_iter4 - final_iter32) * 100. / final_iter32
+    pct_diff8 = (final_iter8 - final_iter32) * 100. / final_iter32
 
     fig, ax = plt.subplots(dpi=125)
-    ax.plot(model.tau, two_final, ls='-', lw=1., alpha=0.4,label='2')
-    ax.plot(model.tau, four_final, ls='-', lw=1., alpha=0.4,label='4')
-    ax.plot(model.tau, eight_final, ls='-', lw=1., alpha=0.4,label='8')
-    ax.plot(model.tau, thirtytwo_final, ls='-', lw=1., alpha=0.4,label='32')
+
+    ax.plot(model.tau, pct_diff2, label='2 points')
+    ax.plot(model.tau, pct_diff4, label='4 points')
+    ax.plot(model.tau, pct_diff8, label='8 points')
+
+    ax.axhline(0., ls='--', color='k')
 
     ax.set_xscale('log')
-    ax.set_yscale('log')
-
-    #ax.set_xlim(-3., np.log10(params['tau_max']))
-    #ax.set_ylim(0.9, 1.2)
 
     ax.set_xlabel(r'$\tau$')
-    ax.set_ylabel('S / B')
-    ax.legend(loc='best')
+    ax.set_ylabel('Percent difference')
+
+    ax.legend()
+
     plt.tight_layout()
-    fn = f'{test_plot_dir}/quad.png'
-    fig.savefig(fn, dpi=125)
-    
-    fig, ax = plt.subplots(dpi=125)
-    ax.plot(model.tau, two_res, label='2')
-    ax.plot(model.tau, four_res, label='4')
-    ax.plot(model.tau, eight_res, label='8')
-    ax.set_xscale('log')
-    ax.set_xlabel(r'$\tau$')
-    ax.set_ylabel('Residual')
-    ax.legend(loc='best')
-    plt.tight_layout()
-    fn = f'{test_plot_dir}/resid.png'
-    fig.savefig(fn, dpi=125)
-    
-    fig, ax = plt.subplots(dpi=125)
-    ax.plot(model.tau, two_perc, label='2')
-    ax.plot(model.tau, four_perc, label='4')
-    ax.plot(model.tau, eight_perc, label='8')
-    ax.set_xscale('log')
-    ax.set_xlabel(r'$\tau$')
-    ax.set_ylabel('% Difference')
-    ax.legend(loc='best')
-    ax.axhline(0, ls='--', color='r')
-    plt.tight_layout()
-    fn = f'{test_plot_dir}/perc_diff.png'
+    fn = f'{test_plot_dir}/quadrature.pdf'
     fig.savefig(fn, dpi=125)
