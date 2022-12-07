99db100e7da17be99b057b511b83724bf6615b98
Anthony Burrow <anthony.r.burrow@gmail.com>
2022-12-07 17:40:38 -0600

    more work on test

--- tests/test_epsilon.py
+++ tests/test_epsilon.py
@@ -12,6 +12,7 @@ params = {
     'T_eff'        : 6000.,
     'max_iter'     : 1000,
     'eps_converge' : 1e-8,
+    'Ng_acceleration' : False,
     'verbose'      : False,
 }
 
@@ -64,41 +65,71 @@ def planck(lam, T):
 
 
 def analytic_J(eps, tau, a=0., b=0.):
-    pass
+    c = np.sqrt(3. * eps)
+    J = a + (b - c * a) * np.exp(-c * tau) / (np.sqrt(3.) + c)
+    return J
 
 
 def plot_J(ax, model, results, eps):
+    if eps == 1.:
+        return
+
     # Find analytic J
     a = planck(5000., params['T_eff'])
     J_analytic = analytic_J(eps, model.tau, a=a)
 
     # Find calculated J
     J_calc = (results - eps * a) / (1. - eps)
+    diffs = np.abs(J_calc[1:] - J_calc[:-1]) / J_calc[:-1]
+    max_diffs = diffs.max(axis=1)
+
+    eps_converge = params['eps_converge']
+    max_iter = params['max_iter']
+    for i in range(max_iter):
+        if max_diffs[i] > eps_converge:
+            continue
+        ind_needed = i
+        break
+    else:
+        ind_needed = max_iter
+
+    J_calc = J_calc[ind_needed]
 
     # Plot here
+    pct_diff = 100. * (J_analytic - J_calc) / J_calc
+
+    ax.plot(model.tau, pct_diff, '-', label=r'\epsilon = ' + f'{eps}')
 
 
 def test_epsilon():
-    fig, ax = plt.subplots(1, 3, figsize=(12, 4), sharey=True)
+    fig1, ax1 = plt.subplots(1, 3, figsize=(12, 4), sharey=True)
+    fig2, ax2 = plt.subplots()
 
     for i, eps in enumerate(eps_to_check):
         params['eps'] = eps
         model = RadModel(params)
         results = model.convergence_test()
 
-        plot_eps(ax[i], model, results, eps)
-        plot_J(ax[i], model, results, eps)
+        plot_eps(ax1[i], model, results, eps)
+        plot_J(ax2, model, results, eps)
 
-    [_ax.set_xlim(params['tau_min'], params['tau_max']) for _ax in ax]
+    # Fig 1 properties
+    [_ax.set_xlim(params['tau_min'], params['tau_max']) for _ax in ax1]
     bottom_convergence = np.sqrt(np.array(eps_to_check).min())
-    ax[0].set_ylim(0.8 * bottom_convergence, 1.2)
+    ax1[0].set_ylim(0.8 * bottom_convergence, 1.2)
 
-    [_ax.set_xscale('log') for _ax in ax]
-    ax[0].set_yscale('log')
+    [_ax.set_xscale('log') for _ax in ax1]
+    ax1[0].set_yscale('log')
 
-    [_ax.set_xlabel(r'$\tau$') for _ax in ax]
-    ax[0].set_ylabel('S / B')
+    [_ax.set_xlabel(r'$\tau$') for _ax in ax1]
+    ax1[0].set_ylabel('S / B')
 
     plt.tight_layout()
     fn = f'{test_plot_dir}/eps_convergence.pdf'
-    fig.savefig(fn)                              	
+    fig1.savefig(fn)                              	
+
+    # Fig 2 properties
+    ax2.set_xscale('log')
+
+    fn = f'{test_plot_dir}/J_comparison.pdf'
+    fig2.savefig(fn)
